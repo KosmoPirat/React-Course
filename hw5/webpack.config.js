4634283be9dee75554a7234760f0d39a1cf92b53
KosmoPirat <kosmopirat@gmail.com>
2019-07-31 17:45:39 +0300

    hw5 in progress 1

--- hw5/webpack.config.js
+++ hw5/webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      },
-      {
-        test: /\.css/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: path.resolve(__dirname, 'dist')
-  }
-}
+    entry: './src/app/App.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+
+        ],
+
+    }
+};
